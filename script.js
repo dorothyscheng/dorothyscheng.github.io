47a296eafa8f5d6078f83ea8f5c9417187965f96
dcheng <cheng.dorothy.s@gmail.com>
2021-06-03 11:21:31 -0600

    Added all sections and updated js scroll function

--- script.js
+++ script.js
@@ -1,24 +1,40 @@
 $(window).scroll(() => {
-    var scroll = $(window).scrollTop(); // how many pixels you've scrolled
-    // var os = $('#main').offset().top; // pixels to the top of main
-    var htMain = $('#main').height(); // height of main in pixels
-    // if you've scrolled further than the top of main plus it's height
-    // change the color. either by adding a class or setting a css property
-    if (scroll >= htMain) {
-        whiteBG();
-        $('#projects-link').addClass('featured');
-    } else {
+    const scroll = $(window).scrollTop(); 
+    const htMain = $('#main').height();
+    const htProjects = $('#projects').height();
+    const osProjects = $('#projects').offset().top;
+    const htSkills = $('#skills').height();
+    const osSkills = $('#skills').offset().top;
+    const htAbout = $('#about').height();
+    const osAbout = $('#about').offset().top;
+    // const htContact = $('#contact').height();
+    // const osContact = $('#contact').offset().top;
+    if (scroll < htMain) {
         blackBG();
         $('#projects-link').removeClass('featured');
+    } else if (scroll >= htMain && scroll < htProjects+osProjects) {
+        whiteBG();
+        $('#projects-link').addClass('featured');
+    } else if (scroll >= htProjects+osProjects && scroll < htSkills+osSkills) {
+        blackBG();
+        $('#skills-link').addClass('featured');
+    } else if (scroll >= htSkills+osSkills && scroll < htAbout+osAbout) {
+        whiteBG();
+        $('#about-link').addClass('featured');
+    } else if (scroll >= htAbout+osAbout) {
+        blackBG();
+        $('#contact-link').addClass('featured');
     };
 });
 
 whiteBG = () => {
     $('#header').addClass('white');
     $('.nav-link').removeClass('white-font');
+    $('.nav-link').removeClass('featured');
 };
 
 blackBG = () => {
     $('#header').removeClass('white');
     $('.nav-link').addClass('white-font');
+    $('.nav-link').removeClass('featured');
 }
